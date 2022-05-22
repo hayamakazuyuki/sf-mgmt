e10161f35a4a501784e1d270a2e3c2023086790a
hayamakazuyuki <hayama@sfinter.com>
2022-05-22 21:12:44 +0900

    shop register added

--- sf/app/customer/forms.py
+++ sf/app/customer/forms.py
@@ -16,3 +16,20 @@ class CustomerForm(FlaskForm):
     address = StringField('住所', validators=[InputRequired('住所は必須です。')])
     bldg = StringField('建物名等')
     is_inactive = BooleanField('休止')
+
+
+class ShopForm(FlaskForm):
+    id = IntegerField('事業所番号', validators=[InputRequired('IDを入力して下さい。'),
+        NumberRange(min=1, max=999999, message='有効なIDを入力して下さい。')])
+    shop_number = StringField('取引先の事業所番号')
+    name = StringField('事業所名', validators=[InputRequired('事業所名は必須です。')])
+    zip = StringField('郵便番号', validators=[InputRequired('郵便番号は必須です。'),
+        Length(min=7, max=7, message='郵便番号は7桁です。')])
+    prefecture = StringField('都道府県', validators=[InputRequired('都道府県は必須です。')])
+    city = StringField('市区町村', validators=[InputRequired('市区町村は必須です。')])
+    town = StringField('町域', validators=[InputRequired('町域は必須です。')])
+    address = StringField('住所', validators=[InputRequired('住所は必須です。')])
+    bldg = StringField('建物名等')
+    is_inactive = BooleanField('休止')
+
+
--- sf/app/customer/views.py
+++ sf/app/customer/views.py
@@ -2,9 +2,9 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 
 from ..extentions import db
 
-from ..models import Customer
+from ..models import Customer, Shop
 
-from .forms import CustomerForm
+from .forms import CustomerForm, ShopForm
 
 customer = Blueprint('customer', __name__, url_prefix='/customer')
 
@@ -67,9 +67,50 @@ def register():
 @customer.route('/<int:id>')
 @customer.route('/<int:id>/<mode>')
 def profile(id, mode=None):
-    customer = Customer.query.get(id)
+    customer = Customer.query.get_or_404(id)
+    page = request.args.get('page', 1, type=int)
+
+    shops = Shop.query.filter_by(customer_id=id).paginate(page=page, per_page=20)
 
     if mode == 'edit':
         return '編集'
 
-    return render_template('customer/profile.html', customer=customer)
+    return render_template('customer/profile.html', customer=customer, shops=shops)
+
+
+@customer.route('/<int:id>/shop/register', methods=['GET', 'POST'])
+def shop_register(id):
+    form = ShopForm()
+    customer = Customer.query.get(id)
+
+    if form.validate_on_submit():        
+        # has to check if the id already exists in the Shop table
+        customer_id = customer.id
+        shop_id = request.form['id']
+        shop_number = request.form.get('shop_number')
+        name = request.form['name']
+        zip = request.form['zip']
+        prefecture = request.form['prefecture']
+        city = request.form['city']
+        town = request.form['town']
+        address = request.form.get('address')
+        bldg = request.form.get('bldg')
+        registered_by = 1
+
+        shop = Shop(customer_id=customer_id, id=shop_id, shop_number=shop_number, name=name, zip=zip,
+             prefecture=prefecture, city=city, town=town, address=address, bldg=bldg, registered_by=registered_by)
+
+        db.session.add(shop)
+    
+        db.session.commit()
+
+        flash('事業所を登録しました。', 'success')
+
+        return redirect(url_for('customer.shop_profile', customer_id=customer_id, id=shop_id))
+
+    return render_template('customer/shop_register.html', customer=customer, form=form)
+
+
+@customer.route('/<int:customer_id>/<int:id>')
+def shop_profile(customer_id, id):
+    return render_template('customer/shop_profile.html')
