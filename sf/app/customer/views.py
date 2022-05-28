2d67e597ff298679e4c324e4f5bcc4ff73ea38b6
hayamakazuyuki <hayama@sfinter.com>
2022-05-29 00:28:09 +0900

    customer and shop update route created

--- sf/app/customer/views.py
+++ sf/app/customer/views.py
@@ -65,7 +65,7 @@ def register():
 
 # show and update customer profile
 @customer.route('/<int:id>')
-@customer.route('/<int:id>/<mode>')
+@customer.route('/<int:id>/<mode>', methods=['GET', 'POST'])
 def profile(id, mode=None):
     customer = Customer.query.get_or_404(id)
     page = request.args.get('page', 1, type=int)
@@ -73,7 +73,25 @@ def profile(id, mode=None):
     shops = Shop.query.filter_by(customer_id=id).paginate(page=page, per_page=20)
 
     if mode == 'edit':
-        return '編集'
+        form = CustomerForm()
+
+        if form.validate_on_submit():
+            customer.name = request.form['name']
+            customer.title = request.form['title']
+            customer.representative = request.form['representative']
+            customer.zip = request.form['zip']
+            customer.prefecture = request.form['prefecture']
+            customer.city = request.form['city']
+            customer.town = request.form['town']
+            customer.address = request.form['address']
+            customer.bldg = request.form['bldg']
+            db.session.commit()
+
+            flash('取引先の情報を更新しました。')
+
+            return redirect(url_for('customer.profile', id=id))
+
+        return render_template('customer/edit.html', form=form, customer=customer)
 
     return render_template('customer/profile.html', customer=customer, shops=shops)
 
@@ -119,8 +137,19 @@ def shop_register(id):
     return render_template('customer/shop_register.html', customer=customer, form=form)
 
 
+# show and update shop profile
 @customer.route('/<int:customer_id>/<int:id>')
-def shop_profile(customer_id, id):
+@customer.route('/<int:customer_id>/<int:id>/<mode>', methods=['GET', 'POST'])
+def shop_profile(customer_id, id, mode=None):
     shop = Shop.query.get_or_404((customer_id, id))
-    
+
+    if mode == 'edit':
+        form = ShopForm()
+
+        if form.validate_on_submit():
+            flash('事業所の情報を更新しました。')
+            return redirect(url_for('customer.shop_profile', customer_id=customer_id, id=id))
+
+        return render_template('customer/shop-edit.html', shop=shop, form=form)
+
     return render_template('customer/shop_profile.html', shop=shop)
