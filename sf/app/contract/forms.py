535da58775c941b95ffc05ab2db307c0b9d7b256
hayamakazuyuki <hayama@sfinter.com>
2022-07-17 15:25:32 +0900

    contract register updated

--- sf/app/contract/forms.py
+++ sf/app/contract/forms.py
@@ -1,6 +1,6 @@
 from unicodedata import name
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, FileField, SelectField
+from wtforms import IntegerField, FileField, SelectField, DateField, BooleanField
 from wtforms.validators import NumberRange, DataRequired
 from flask_wtf.file import FileAllowed
 
@@ -15,6 +15,9 @@ class ContractRegisterForm(FlaskForm):
     contractor_id = IntegerField('パートナーID', validators=[DataRequired('パートナーIDは必須です。'),
         NumberRange(min=1, max=99999, message='有効なIDを入力してください。')])
     item_id = SelectField('契約種類', coerce=int)
+    effective_from = DateField('契約有効開始日')
+    expires_on = DateField('契約終了日')
+    auto_extention = BooleanField('自動更新')
     contract_copy = FileField('契約書コピー', validators=[FileAllowed('pdf', '登録可能なファイルはPDFのみです。')])
 
     def __init__(self, *args, **kwargs):
