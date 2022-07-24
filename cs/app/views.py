6fae42a0225d987161cce308609ed9afd677d437
hayamakazuyuki <hayama@sfinter.com>
2022-07-24 22:42:07 +0900

    cs index and home separated

--- cs/app/views.py
+++ cs/app/views.py
@@ -5,3 +5,7 @@ view = Blueprint('view', __name__)
 @view.route('/')
 def index():
     return render_template('index.html')
+
+@view.route('/home')
+def home():
+    return render_template('home.html')
