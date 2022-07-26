26e226f7f8bcda4b59627a2860d4e0c83252d5b8
hayamakazuyuki <hayama@sfinter.com>
2022-07-26 23:58:59 +0900

    cs shop route updated

---
+++ cs/app/models.py
@@ -0,0 +1,27 @@
+from .extentions import db
+
+class Customer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    title = db.Column(db.String(50), nullable=True)
+    representative = db.Column(db.String(50), nullable=False)
+    zip = db.Column(db.String(7), nullable=False)
+    prefecture = db.Column(db.String(4), nullable=False)
+    city = db.Column(db.String(50), nullable=False)
+    town = db.Column(db.String(50), nullable=False)
+    address = db.Column(db.String(100))
+    bldg = db.Column(db.String(50))
+    shops = db.relationship('Shop', backref=db.backref('customer', lazy=True))
+
+
+class Shop(db.Model):
+    customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'), primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    shop_number = db.Column(db.String(15), nullable=True)
+    name = db.Column(db.String(100), nullable=False)
+    zip = db.Column(db.String(7), nullable=False)
+    prefecture = db.Column(db.String(4), nullable=False)
+    city = db.Column(db.String(50), nullable=False)
+    town = db.Column(db.String(50), nullable=False)
+    address = db.Column(db.String(100))
+    bldg = db.Column(db.String(50))
