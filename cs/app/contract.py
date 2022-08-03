cd41b7b38dab4581ab0d575fffee0b7ec9138623
hayamakazuyuki <hayama@sfinter.com>
2022-08-03 13:48:38 +0900

    cs google-cloud installed

--- cs/app/contract.py
+++ cs/app/contract.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template, request
+import os, io
+
+from flask import Blueprint, render_template, request, current_app, send_file, abort
 
 from .models import Contract, Shop
 
@@ -12,3 +14,32 @@ def contract_details(id):
 
     return render_template('contract/contract-details.html', contract=contract, shop=shop)
     
+
+@contract.route('/get_copy/<int:id>')
+def get_copy(id):
+
+    contract = Contract.query.get(id)
+
+    try:
+        customer_id = str(contract.customer_id)
+        shop_id = str(contract.shop_id)
+        contractor_id = str(contract.contractor_id)
+        contract_id = str(contract.id)
+
+        bucket_name = current_app.config['GCS_BUCKET_NAME']
+
+        storage_client = storage.Client()
+
+        bucket = storage_client.bucket(bucket_name)
+        blob = bucket.blob('contract/' + customer_id + '/' + shop_id + '/' + contractor_id + '/' + contract_id + '.pdf')
+        pdf_binary = blob.download_as_bytes()
+
+        return send_file(
+            io.BytesIO(pdf_binary),
+            mimetype='application/pdf',
+            as_attachment=False,
+            attachment_filename='test.pdf'
+        )
+
+    except FileNotFoundError:
+        abort(404)
