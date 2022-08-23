a4d3a39e095f6e3c74dea3fff7b99b231f0d89c4
hayamakazuyuki <hayama@sfinter.com>
2022-08-23 23:39:40 +0900

    sf contractor profile updated

--- sf/app/contractor/views.py
+++ sf/app/contractor/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for, current_app
+from flask import Blueprint, render_template, request, flash, redirect, url_for, current_app, jsonify
 
 from ..extentions import db, storage
 
