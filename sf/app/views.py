7c352cd169c5a8e37b93d15d0a2d388f9973b11b
hayamakazuyuki <hayama@sfinter.com>
2022-11-14 07:24:32 +0900

    sf komeri mock

--- sf/app/views.py
+++ sf/app/views.py
@@ -5,3 +5,11 @@ top = Blueprint('top', __name__)
 @top.route('/')
 def index():
     return render_template('index.html')
+
+@top.route('/komeri')
+def komeri():
+    return render_template('komeri.html')
+
+@top.route('/terms')
+def terms():
+    return render_template('terms.html')
